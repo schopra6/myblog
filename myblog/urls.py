60c4d93c93acfbe14e24f34131c1bd2bdffead1b
sahilc.mac <akonchopra@gmail.com>
2018-04-01 00:26:34 +0530

    ff

--- myblog/urls.py
+++ myblog/urls.py
@@ -24,9 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('zinnia.urls')),
-     url(r'^comments/', include('django_comments.urls')),
-
-     url(r'^', include('zinnia.urls')),
+    url(r'^blog', include('zinnia.urls')),
      url(r'^comments/', include('django_comments.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
