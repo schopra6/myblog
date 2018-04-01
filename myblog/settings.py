d3f7d21d5bb026a10be9b9a2c56a331da98fef31
sahilc.mac <akonchopra@gmail.com>
2018-04-01 19:13:25 +0530

    ddd

--- myblog/settings.py
+++ myblog/settings.py
@@ -169,5 +169,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-SITE_ID=8
+SITE_ID=2
 django_heroku.settings(locals())
